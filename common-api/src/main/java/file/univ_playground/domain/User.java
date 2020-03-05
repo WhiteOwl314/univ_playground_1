dbe5b6ef0f470ec50d6b5e0bb875e2b24db5347d
WhiteOwl314 <parksj914@naver.com>
2020-03-05 15:08:09 +0900

    Design User

---
+++ common-api/src/main/java/file/univ_playground/domain/User.java
@@ -0,0 +1,32 @@
+package file.univ_playground.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Column(name = "Comunity_id")
+    private Long comunityId;
+}
