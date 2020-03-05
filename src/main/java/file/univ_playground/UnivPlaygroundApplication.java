05aa64d72b32575090da525e73bddd9c797ea7ae
WhiteOwl314 <parksj914@naver.com>
2020-03-05 14:45:14 +0900

    Initial commit

---
+++ src/main/java/file/univ_playground/UnivPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package file.univ_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnivPlaygroundApplication.class, args);
+    }
+
+}
